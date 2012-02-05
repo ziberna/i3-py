1bdda450fac45c78e27a6b36f2e7b95883367527
Jure Ziberna <j.ziberna@gmail.com>
2012-02-05 13:32:42 +0100

    packaging

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+long_description = """
+i3-py is a Python module for communicating with i3 window manager.
+"""
+
+setup(
+    name='i3-py',
+    description='Python module for communicating with i3 window manager',
+    long_description=long_description,
+    author='Jure Ziberna',
+    author_email='j.ziberna@gmail.com',
+    url='https://github.com/jzib/i3-py',
+    version='0.2.4',
+    license='GNU GPL 3',
+    py_modules=['i3']
+)
