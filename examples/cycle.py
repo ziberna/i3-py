5bc09ecd7574272df0ea7c5eebaf77d64ca43b14
Jure Ziberna <j.ziberna@gmail.com>
2012-04-02 01:26:53 +0200

    cycle: comments

--- examples/cycle.py
+++ examples/cycle.py
@@ -2,11 +2,15 @@ import i3
 import time
 
 def cycle():
+    # get currently focused windows
     current = i3.filter(nodes=[], focused=True)
+    # get unfocused windows
     other = i3.filter(nodes=[], focused=False)
+    # focus each previously unfocused window for 0.5 seconds
     for window in other:
         i3.window('focus', title=window['name'])
         time.sleep(0.5)
+    # focus the original windows
     for window in current:
         i3.window('focus', title=window['name'])
 
