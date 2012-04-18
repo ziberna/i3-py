9b92f05f2f01ed8ce510059fdbee58df382f3921
Jure Ziberna <j.ziberna@gmail.com>
2012-04-18 05:18:36 +0200

    packaging update: 0.6.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     author='Jure Ziberna',
     author_email='j.ziberna@gmail.com',
     url='https://github.com/ziberna/i3-py',
-    version='0.5.5.1',
+    version='0.6.0',
     license='GNU GPL 3',
     py_modules=['i3']
 )
